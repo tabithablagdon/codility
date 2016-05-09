ac47288bbafbaa6a5aed2079df787bb99b92e275
Tabitha Blagdon <tjlay23@gmail.com>
2016-05-09 14:56:04 -0700

    Update lesson6-sorting-maxProductOf3.js

--- lesson6-sorting-maxProductOf3.js
+++ lesson6-sorting-maxProductOf3.js
@@ -1,9 +1,8 @@
 /*
 Lesson 6: Sorting - Max Product of 3
+Score: Correctioness 100%, Performance 100% (O(N*log(N)) time complexity)
 
-Task: Maximize A[P] * A[Q] * A[R] for any triplet (P, Q, R).
-
-A non-empty zero-indexed array A consisting of N integers is given. The product of triplet (P, Q, R) equates to A[P] * A[Q] * A[R] (0 ≤ P < Q < R < N).
+Task: Maximize A[P] * A[Q] * A[R] for any triplet (P, Q, R).  A non-empty zero-indexed array A consisting of N integers is given. The product of triplet (P, Q, R) equates to A[P] * A[Q] * A[R] (0 ≤ P < Q < R < N).
 
 For example, array A such that:
 
@@ -45,3 +44,15 @@ expected worst-case time complexity is O(N*log(N));
 expected worst-case space complexity is O(1), beyond input storage (not counting the storage required for input arguments).
 Elements of input arrays can be modified.
 */
+
+function solution(A) {
+  var len = A.length;
+  A.sort(function(a, b) {
+    return a - b;
+  });
+  return Math.max(A[0]*A[1]*A[len-1], A[len-3]*A[len-2]*A[len-1])
+}
+
+var arr = [-4, -6, 3, 4, 5];
+solution(arr);
+// => 120
